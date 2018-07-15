ea28a8a91f55853c504ce4c74ce3c4d54bf01c13
Christopher Kilding <user@fake>
2018-07-15 21:31:11 +0100

    Use loadtest4j 0.12.0

--- src/test/java/com/github/loadtest4j/example/driver/DummyResponseTime.java
+++ src/test/java/com/github/loadtest4j/example/driver/DummyResponseTime.java
@@ -1,10 +1,10 @@
 package com.github.loadtest4j.example.driver;
 
-import com.github.loadtest4j.loadtest4j.ResponseTime;
+import com.github.loadtest4j.loadtest4j.driver.DriverResponseTime;
 
 import java.time.Duration;
 
-class DummyResponseTime implements ResponseTime {
+class DummyResponseTime implements DriverResponseTime {
     @Override
     public Duration getPercentile(int i) {
         return Duration.ZERO;
